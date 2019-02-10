a0bb7a5bf1ef5fc1c545fef9229bd2564bb156ec
Jason Ross <algorythm@gmail.com>
2019-02-09 19:40:32 -0700

    updated to grab a sha1 hash of the screenshot. prepping to avoid saving dupes

--- url2screen.js
+++ url2screen.js
@@ -1,50 +1,60 @@
-"use strict";
+const fs = require('fs')
+const readline = require('readline')
+const stream = require('stream')
+const screenie = require('node-server-screenshot')
+const crypto = require('crypto')
+const path = require('path')
 
-const fs = require('fs');
-const readline = require('readline');
-const stream = require('stream');
-const screenie = require("node-server-screenshot");
+const outDir = path.join(__dirname, '/screenshots')
+const inFile = './urls.txt'
 
-const outDir = __dirname + '/screenshots';
-const inFile = './urls.txt';
+// parse the urls file and push each url into an array
+function load (inFile, cb) {
+  const outStream = new stream()
+  const inStream = fs.createReadStream(inFile)
+  var rl = readline.createInterface(inStream, outStream)
+  var arr = []
 
-function load(inFile, cb) {
-  const outStream = new stream;
-  const inStream = fs.createReadStream(inFile);
-  var rl = readline.createInterface(inStream, outStream);
-  var arr = [];
+  rl.on('line', function (line) {
+    arr.push(line)
+  })
 
-  rl.on('line', function(line) {
-    arr.push(line);
-  });
-
-  rl.on('close', function() {
-    console.log('done loading file: ', inFile);
-    cb(arr, outDir);
-  });
+  rl.on('close', function () {
+    console.log('done loading file: ', inFile)
+    cb(arr, outDir)
+  })
 }
 
-function capture(urls, outDir) {
-  
+// take a screenshot of the urls in the array
+function capture (urls, outDir) {
   // create the outDir if it doesn't exist
-  if (!fs.existsSync(outDir)){
-    fs.mkdirSync(outDir);
+  if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir)
   }
-  
-  for (var i=0; i < urls.length; i++) {
-    const targetUrl = urls[i];
-    const proto = targetUrl.split(':', 1)[0];
-    const domainName = targetUrl.split('//', 3)[1];
-    const outFile = outDir + '/' + proto + '_' + domainName + '.jpg';
-  
-    screenie.fromURL(targetUrl, outFile, function(){
-        console.log("saved screenshot of " + domainName + " at " + outFile);
-    });
+
+  for (var i = 0; i < urls.length; i++) {
+    const targetUrl = urls[i]
+    const proto = targetUrl.split(':', 1)[0]
+    const domainName = targetUrl.split('//', 3)[1]
+    const outFile = outDir + '/' + proto + '_' + domainName + '.jpg'
+
+    screenie.fromURL(targetUrl, outFile, function () {
+      console.log('saved screenshot of ' + domainName + ' at ' + outFile)
+      // get the hash of the saved file so we avoid dupes
+      var fd = fs.createReadStream(outFile)
+      const hash = crypto.createHash('sha1')
+      hash.setEncoding('hex')
+      fd.on('end', function () {
+        hash.end()
+        console.log('hash: ', hash.read())
+      })
+      fd.pipe(hash)
+    })
   }
 }
 
-function main() {
-  load(inFile, capture);
+function main () {
+  load(inFile, capture)
 }
 
-main();
+main()
